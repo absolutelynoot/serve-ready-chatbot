0495b2feeeea30106f0ebbdcc9a892c2db227ff6
Ming Rong <kwekmingrong@yahoo.com.sg>
2023-10-09 18:24:41 +0800

    added some logo and format

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
+from PIL import Image
 
 # 1. Load the document
 loader = PyPDFDirectoryLoader("./docs")
@@ -34,13 +35,16 @@ vectordb = Chroma.from_documents(
 )
 
 def main():
-    st.set_page_config(page_title="A Chatbot created to dive into the world of Migrant Workers!", page_icon=":robot_face:", layout="wide")
+    st.set_page_config(page_title="A Chatbot created to dive into the world of Migrant Workers!", page_icon=":robot_face:", layout="centered")
+    image = Image.open('HealthServe_Logo.png')
+    col1, col2, col3 = st.columns([0.2, 5, 0.2])
+    col2.image(image, use_column_width=True)
     st.header("A Chatbot created to dive into the world of Migrant Workers!" + " :robot_face:")
     st.subheader("Ask a question about migrant workers in Singapore and the chatbot will try to answer it!")
     user_question = st.text_area("Ask a burning question you have 🔥")
 
     if user_question:
-        docs = vectordb.search(user_question, search_type="mmr")
+        docs = vectordb.search(user_question, search_type="mmr", search_kwargs={"k": 10})
         llm = OpenAI()
         chain = load_qa_chain(llm, chain_type="stuff")
         response = chain.run(input_documents=docs, question=user_question)
