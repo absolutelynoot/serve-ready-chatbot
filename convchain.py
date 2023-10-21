b80600b390d25d4ce09ec980c38581236b959802
Faisal Samudra <isalsamudra@hotmail.com>
2023-10-22 02:13:40 +0800

    added chat history

---
+++ convchain.py
@@ -0,0 +1,59 @@
+import param
+
+# Defines a class named cbfs with methods to handle various functions of a chatbot
+class cbfs(param.Parameterized):
+    chat_history = param.List([])
+    answer = param.String("")
+    db_query  = param.String("")
+    db_response = param.List([])
+
+# Initializers the cbfs instance and calls load_qa to return a chain
+    def __init__(self,  **params):
+        super(cbfs, self).__init__( **params)
+        self.qa = load_qa()
+
+# Method to handle conversation flow by taking in a query (or question),
+# processes it using the qa function, updates chat history, and generates appropriate responses.
+    def convchain(self, query):
+        if not query:
+            return pn.WidgetBox(pn.Row('User:', pn.pane.Markdown("", width=600)), scroll=True)
+        result = self.qa({"question": query, "chat_history": self.chat_history})
+        self.chat_history.extend([(query, result["answer"])])
+        self.db_query = result["generated_question"]
+        self.db_response = result["source_documents"]
+        self.answer = result['answer']
+        return 
+
+    @param.depends('db_query ', )
+    def get_lquest(self):
+        if not self.db_query :
+            return pn.Column(
+                pn.Row(pn.pane.Markdown(f"Last question to DB:", styles={'background-color': '#F6F6F6'})),
+                pn.Row(pn.pane.Str("no DB accesses so far"))
+            )
+        return pn.Column(
+            pn.Row(pn.pane.Markdown(f"DB query:", styles={'background-color': '#F6F6F6'})),
+            pn.pane.Str(self.db_query )
+        )
+
+    @param.depends('db_response', )
+    def get_sources(self):
+        if not self.db_response:
+            return
+        rlist=[pn.Row(pn.pane.Markdown(f"Result of DB lookup:", styles={'background-color': '#F6F6F6'}))]
+        for doc in self.db_response:
+            rlist.append(pn.Row(pn.pane.Str(doc)))
+        return pn.WidgetBox(*rlist, width=600, scroll=True)
+
+    @param.depends('convchain', 'clr_history')
+    def get_chats(self):
+        if not self.chat_history:
+            return pn.WidgetBox(pn.Row(pn.pane.Str("No History Yet")), width=600, scroll=True)
+        rlist=[pn.Row(pn.pane.Markdown(f"Current Chat History variable", styles={'background-color': '#F6F6F6'}))]
+        for exchange in self.chat_history:
+            rlist.append(pn.Row(pn.pane.Str(exchange)))
+        return pn.WidgetBox(*rlist, width=600, scroll=True)
+
+    def clr_history(self,count=0):
+        self.chat_history = []
+        return
--- telegram_bot.py
+++ telegram_bot.py
@@ -10,6 +10,10 @@ TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
 bot.set_webhook()
 
+# Dictionary to store conversation history
+conversation_history = {}
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
     """
@@ -27,12 +31,30 @@ def start(message):
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    response = model.getResponse(message.text)
-    bot.send_message(message.chat.id, response)
+
+    # Retrieve the user id and the message from telegram API
+    user_id = message.from_user.id
+    question = message.text
+
+    # Retrieve the conversation history for the user
+    chat_history = conversation_history.get(user_id, [])
+
+    #  Get the response from the model
+    response = model.getResponse(question, chat_history)
+
+    # Add the model's response to the conversation history
+    chat_history.append((question, response['answer']))
+
+    print("====== chat history =======")
+    print(chat_history)
+
+    # Store the updated conversation history for the user
+    conversation_history[user_id] = chat_history
+
+    bot.send_message(message.chat.id, response['answer'])
 
 def main():
     """Runs the Telegram Bot"""
-    print('Loading configuration...') # Perhaps an idea on what you may want to change (optional)
     print('Successfully loaded! Starting bot...')
     bot.infinity_polling()
 
